f2f601b0cbf40f5b6d6ba0685284258671f41b64
Amin Saba <amn.brhm.sb@gmail.com>
2022-01-22 13:55:56 +0330

    clean up sbtest

--- xactl/tests/sbtest_p.cpp
+++ xactl/tests/sbtest_p.cpp
@@ -9,13 +9,26 @@
 #include "sbtest_common.h"
 
 
+
+/*
+ * Test Structure
+ * 1- Create file 'TARGET_FILE' and make system call to XAC module
+ *    to whitelist it for READ access.
+ * 2- Activate the sandbox
+ * 3- Run the target executable which verifies sandbox functionality
+ */
+
+
 int main(int argc, char *argv[])
 {
 	int rc = 0;
-
 	struct selfbox_args arg;
 
-	touch_file(TARGET_FILE);
+	rc = touch_file(TARGET_FILE);
+	if (rc) {
+		perror("touch target file");
+		exit(1);
+	}
 
 	rc = xacsb_allow_path(TARGET_FILE, VREAD);
 	if (rc) {
